6fbcb67d7b3d04e191b0967cb74281a3fd89cd72
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-05-17 18:43:08 +0200

    [REFACTOR] Better implementation of logger

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,21 @@
 var winston = require('winston');
 
+// Add colors to winston
+winston.addColors({
+    debug: 'green',
+    error: 'red',
+    info: 'blue',
+    warn: 'yellow'
+});
+
 var logger = new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)()
+        new (winston.transports.Console)({
+            level: (process.env.NODE_ENV === 'production') ? 'warn' : 'silly',
+            colorize: true
+        })
     ]
 });
 
+// Generate the logger
 module.exports = logger;
